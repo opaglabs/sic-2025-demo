8a57f0d4ec7a0361a1af2596a43e02f785e914d7
Otávio Paganotti <otavio@opag.com.br>
2025-08-24 21:19:38 -0400

    chore: adds @nuxthub/core module with hub features

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui']
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxthub/core'],
+  hub: {
+    analytics: true,
+    database: true,
+  }
 })
